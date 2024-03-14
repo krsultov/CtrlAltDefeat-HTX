22d182b4ffaf0b702a8b4b4a086b8ecd07731f42
Krum <krsultov@gmail.com>
2024-03-14 20:10:59 +0200

    fix background 2

--- ctrlaltdefeat-htx/tailwind.config.js
+++ ctrlaltdefeat-htx/tailwind.config.js
@@ -14,8 +14,8 @@ module.exports = {
         secondary:"#FFBF69",
         primaryDark:"#2EC4B6",
         secondaryDark:"#FF9F1C",
-        bgPrimary: 'rgba(83, 199, 240, 0.6)',
-        bgSecondary: 'rgba(250, 242, 195, 0.6)',
+        bgPrimary: 'rgba(83, 199, 240, 0.3)',
+        bgSecondary: 'rgba(250, 242, 195, 0.3)',
       }
     },
   },
