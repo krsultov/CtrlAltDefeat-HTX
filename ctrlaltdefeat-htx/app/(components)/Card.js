b74263dea46342638061a3bc483a0235f3c0842e
VictorBotev12 <160576550+VictorBotev12@users.noreply.github.com>
2024-03-14 15:11:50 +0200

    Implement Card and Events page

--- ctrlaltdefeat-htx/app/(components)/Card.js
+++ ctrlaltdefeat-htx/app/(components)/Card.js
@@ -1,6 +1,6 @@
 const Card = ({image, title, description, location, date, organizer}) => {
     return (
-      <div className="max-w-md bg-white rounded overflow-hidden shadow-lg mx-auto">git pull
+      <div className="max-w-md bg-white rounded overflow-hidden shadow-lg mx-auto">
         <img className="w-full" src={image} alt="Placeholder" />
         <div className="px-6 py-4 space-y-3">
           <div className="font-bold text-2xl mb-2">{title}</div>
@@ -11,7 +11,7 @@ const Card = ({image, title, description, location, date, organizer}) => {
           </div>
         </div>
         <div className="px-6 pt-4 pb-2 flex justify-between items-center gap-8">
-          <button className="bg-orange-300 hover:bg-orange-400 text-white font-bold py-2 px-4 rounded">
+          <button className="bg-secondary hover:bg-secondaryDark text-white font-bold py-2 px-4 rounded">
             Бутон
           </button>
           
