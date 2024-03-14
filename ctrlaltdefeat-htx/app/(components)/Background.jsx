9f3c1bb5a75123c89a02bd099043b30874f5bb92
krsultov <krsultov@gmail.com>
2024-03-15 00:35:40 +0200

    improve home improve login and signup fix background

--- ctrlaltdefeat-htx/app/(components)/Background.jsx
+++ ctrlaltdefeat-htx/app/(components)/Background.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 function Background() {
     return (
         <div
-            className="opacity-40 fixed top-0 left-0 w-full h-full bg-gradient-to-br z-[-1] from-bgPrimary to-bgSecondary flex items-center justify-center overflow-hidden">
+            className="opacity-90 fixed top-0 left-0 w-full h-full bg-gradient-to-br z-[-1] from-bgPrimary to-bgSecondary flex items-center justify-center overflow-hidden">
         </div>
     );
 }
--- ctrlaltdefeat-htx/app/page.js
+++ ctrlaltdefeat-htx/app/page.js
@@ -1,21 +1,33 @@
-import Image from "next/image";
 import Link from "next/link";
-import LinkButton from "./(components)/LinkButton";
-import ContentWrapper from "./(components)/ContentWrapper";
 
 export default function Home() {
     return (
-        <div className="relative text-white">
-            <section className="py-12 bg-black bg-opacity-25">
+        <div className="text-white">
+            <section className="py-12 bg-black bg-opacity-10">
                 <div className="container mx-auto px-4 py-16">
-                    <h1 className="text-4xl font-bold mb-4">Save Our Sea!</h1>
-                    <p className="text-lg mb-6">
-                        Join the fight to protect our oceans. Report problems, organize events, and make a difference.
-                    </p>
-                    <button
-                        className="bg-primaryDark hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondaryDark text-white font-semibold py-3 px-6 rounded-md">
-                        Sign Up
-                    </button>
+                    <div>
+                        <h1 className="text-6xl text-bgSecondary font-bold mb-10">Baywatch</h1>
+                        <p className="text-xl sm:text-xl mb-6">
+                            <b>Tired of seeing trash in your favorite places? We are too!</b> That's why we created this
+                            app
+                            – to empower everyday people to take action for a greener planet.</p>
+                    </div>
+                    <div>
+                        <h1 className="text-xl font-semibold text-bgSecondary mb-2">With Baywatch you can:</h1>
+                        <ul className="ml-10 list-disc text-lg mb-6">
+                            <li>Create reports for polluted areas and other issues</li>
+                            <li>Find or create local cleanup events</li>
+                            <li>Earn points and level up your volunteer status</li>
+                            <li>See your impact with before/after photos</li>
+                            <li>Connect with a community of like-minded people</li>
+                        </ul>
+                    </div>
+                    <Link href={"/signup"}>
+                        <button
+                            className="bg-secondary hover:bg-secondaryDark focus:outline-none focus:ring-0 focus:ring-offset-1 focus:ring-secondaryDark text-black font-semibold py-3 px-6 rounded-md">
+                            Sign Up
+                        </button>
+                    </Link>
                 </div>
             </section>
 
@@ -43,16 +55,32 @@ export default function Home() {
                 </div>
             </section>
 
-            <section className="py-12 bg-black bg-opacity-25">
+            <section className="py-12 bg-black bg-opacity-10">
                 <div className="container mx-auto px-4 py-16">
-                    <h1 className="text-4xl font-bold mb-4">Save Our Sea!</h1>
+                    <h1 className="text-4xl font-bold mb-4">Noticed something?</h1>
                     <p className="text-lg mb-6">
-                        Join the fight to protect our oceans. Report problems, organize events, and make a difference.
+                        You can create a report or create an event to help the environment!
                     </p>
-                    <button
-                        className="bg-primaryDark hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondaryDark text-white font-semibold py-3 px-6 rounded-md">
-                        Sign Up
-                    </button>
+                    <div className="flex space-x-5">
+                        <Link href={"/#reports"}>
+                            <button
+                                className="bg-secondary focus:outline-none focus:ring-0 focus:ring-offset-1 focus:ring-secondaryDark hover:bg-secondaryDark text-black font-semibold py-3 px-6 rounded-md">
+                                Create a Report
+                            </button>
+                        </Link>
+                        <Link href={"/signup"}>
+                            <button
+                                className="bg-secondary focus:outline-none focus:ring-0 focus:ring-offset-1 focus:ring-secondaryDark hover:bg-secondaryDark text-black font-semibold py-3 px-6 rounded-md">
+                                Sign Up
+                            </button>
+                        </Link>
+                        <Link href={"/events"}>
+                            <button
+                                className="bg-secondary focus:outline-none focus:ring-0 focus:ring-offset-1 focus:ring-secondaryDark hover:bg-secondaryDark text-black font-semibold py-3 px-6 rounded-md">
+                                View Events
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </section>
 
