eadb642ba79006fef4415090f116ebd7b7d2a1e4
Krum <krsultov@gmail.com>
2024-03-14 11:08:26 +0200

    Navigation component

--- ctrlaltdefeat-htx/app/(components)/Navigation.jsx
+++ ctrlaltdefeat-htx/app/(components)/Navigation.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import Link from "next/link";
 
 const Navigation = () => {
     return (
-        <div>Navigation</div>
+        <nav className="flex justify-between bg-blue-200 p-4">
+            <div className="flex items-center space-x-4">
+                <Link href="/">
+                    LOGO + NAME
+                </Link>
+            </div>
+            <div className="flex items-center space-x-4">
+                <Link href="/#events">
+                    Events
+                </Link>
+                <Link href="/#reports">
+                    Reports
+                </Link>
+                <Link href="/#login">
+                    Login
+                </Link>
+            </div>
+        </nav>
     );
 };
 
