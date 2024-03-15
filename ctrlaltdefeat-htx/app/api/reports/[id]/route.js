89f06fd99ad4e88f464933274423109e4869dbd6
Krum <krsultov@gmail.com>
2024-03-15 10:41:23 +0200

    Update report by id - PUT /api/reports/:id

--- ctrlaltdefeat-htx/app/api/reports/[id]/route.js
+++ ctrlaltdefeat-htx/app/api/reports/[id]/route.js
@@ -11,3 +11,20 @@ export async function GET(req, {params}){
         return NextResponse.json({message: "Error", e}, {status:500})
     }
 }
+
+// Update Report by Id
+export async function PUT(req, {params}){
+    try{
+        const {id} = params
+        const body = await req.json()
+        const reportData = body.formData
+
+        const updatedReport = await Report.findByIdAndUpdate(id, {
+            ...reportData
+        })
+
+        return NextResponse.json({message: "Event updated successfully"}, {status:200})
+    } catch (e) {
+        return NextResponse.json({message: "Error", e}, {status:500})
+    }
+}
