4c351497bc630bd58f7f47d3b4ecd21753faa6a9
Krum <krsultov@gmail.com>
2024-03-15 10:14:47 +0200

    Create new report - POST /api/reports

---
+++ ctrlaltdefeat-htx/app/api/reports/route.js
@@ -0,0 +1,17 @@
+import Report from "@/app/(models)/EventModel";
+import {NextResponse} from "next/server";
+
+// Create new report
+export async function POST(req) {
+    try {
+        const body = await req.json()
+        const reportData = body.formData
+        await Report.create(reportData)
+
+        return NextResponse.json({message: "Report Created Successfully"}, {status: 201})
+    } catch (e) {
+        console.log(e)
+        return NextResponse.json({message: "Error", e}, {status: 500})
+    }
+}
+
