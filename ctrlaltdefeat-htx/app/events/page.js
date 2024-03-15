d65156af70c56a5af46f4da014f2ef0301ffc610
Krum <krsultov@gmail.com>
2024-03-15 17:20:12 +0200

    fix eventlist

--- ctrlaltdefeat-htx/app/events/page.js
+++ ctrlaltdefeat-htx/app/events/page.js
@@ -16,54 +16,32 @@ const getEvents = async () => {
 
 const Events = async () => {
 
-  const {events} = await getEvents()
+    const {events} = await getEvents()
 
-  return (
-    <div className="flex gap-3 lg:flex-wrap p-8">
-      <Card
-        title="Място"
-        image="https://i.natgeofe.com/k/cd784533-e5ef-439a-8167-2ba61b0a9a4b/wave_16x9.jpg?w=1200"
-        description="Така ще ни изглеждат хубавите картички"
-        location="Варна, плаж Хубав"
-        date="26.07.2024"
-        organizer="Цветомир Александров"
-        id="1"
-      />
-      <Card
-        title="Място"
-        image="https://i.natgeofe.com/k/cd784533-e5ef-439a-8167-2ba61b0a9a4b/wave_16x9.jpg?w=1200"
-        description="Така ще ни изглеждат хубавите картички"
-        location="Варна, плаж Хубав"
-        date="26.07.2024"
-        organizer="Цветомир Александров"
-        id="2"
-      />
-      <Card
-        title="Място"
-        image="https://i.natgeofe.com/k/cd784533-e5ef-439a-8167-2ba61b0a9a4b/wave_16x9.jpg?w=1200"
-        description="Така ще ни изглеждат хубавите картички"
-        location="Варна, плаж Хубав"
-        date="26.07.2024"
-        organizer="Цветомир Александров"
-        id="3"
-      />
-      <div className="p-5">
-      <div>
-          {events && uniqueStatuses?.map((uniqueStatus, statusIndex) => (
-              <div key={statusIndex} className="mb-4">
-                  <h2>{uniqueStatus}</h2>
-                  <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-                      {events.filter((event) => event.category === uniqueStatus).map((filteredEvent, _index) => (
-                          <Card id={_index} key={_index} ticket={filteredEvent}/>
-                      ))}
-                  </div>
+    const uniqueStatuses = [
+        ...new Set(events?.map(({status}) => status))
+    ]
 
-              </div>
-          ))}
-      </div>
-    </div>
-    </div>
-  );
+    return (
+        <div className="flex gap-3 lg:flex-wrap p-8">
+
+            <div className="p-5">
+                <div>
+                    {events && uniqueStatuses?.map((uniqueStatus, statusIndex) => (
+                        <div key={statusIndex} className="mb-4">
+                            <h2>{uniqueStatus}</h2>
+                            <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+                                {events.filter((event) => event.category === uniqueStatus).map((filteredEvent, _index) => (
+                                    <Card id={_index} key={_index} ticket={filteredEvent}/>
+                                ))}
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Events;
