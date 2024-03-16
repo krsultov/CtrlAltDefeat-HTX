84b312c84a277750c80a9b205fb44504bbc8a45e
krsultov <krsultov@gmail.com>
2024-03-16 06:05:18 +0200

    improve event details page

--- ctrlaltdefeat-htx/app/events/[id]/page.js
+++ ctrlaltdefeat-htx/app/events/[id]/page.js
@@ -36,7 +36,7 @@ export default function EventDetails({params}) {
 
         console.log(JSON.stringify(participant))
 
-        const modifiedParticipants = [...event.participants,participant]
+        const modifiedParticipants = [...event.participants, participant]
 
         const modifiedEvent = {...event, participants: modifiedParticipants}
 
@@ -49,35 +49,42 @@ export default function EventDetails({params}) {
     }
 
     return event && (
+        <div className="flex justify-center items-center h-full w-full ">
+            <div
+                className=" flex flex-col bg-gray-200/40 justify-between space-y-10 rounded-lg shadow-lg p-8 text-gray-600 border border-cyan-500/50 w-full md:w-11/12">
 
-        <div className="grid grid-cols-1 md:grid-cols-2">
-            <img src={event.beforeImage}
-                 alt='Event picture' className="p-4 w-full">
-            </img>
-            <div>
-                <p className="pt-3 text-slate-800 text-lg">{event.location}</p>
-                <h1 className="text-7xl py-10">{event.title}</h1>
-                <p>{event.description}</p>
-                <button onClick={() => addParticipant(currentUser.userContent._id,currentUser.userContent.username, currentUser.userContent.profilePicture)}
-                        className="  my-10 border py-2 border-solid border-base-500 rounded-lg px-5 bg-secondary hover:bg-secondaryDark">Join
-                    this event
-                </button>
-                <div className="space-x-2 flex">
-                    <p className="space-x-2 py-0.5 pr-5">Participants: </p>
-                    {event.participants.map((participant, _index) => (
-                        <div className="flex items-center gap-3" key={_index}>
-                            <a className="space-x-2 py-0.5">{participant.name}</a>
-                            <div
-                                className="flex  h-10 w-10 rounded-full overflow-hidden mr-6 border-2 border-emerald-500/50">
-                                <img src={participant.pfp} alt="Profile"/>
-                            </div>
+                <div className="grid grid-cols-1 md:grid-cols-2">
+                    <img src={event.beforeImage}
+                         alt='Event picture' className="p-4 w-full">
+                    </img>
+                    <div>
+                        <p className="pt-3 text-slate-800 text-lg">{event.location}</p>
+                        <h1 className="text-7xl py-10">{event.title}</h1>
+                        <p>{event.description}</p>
+                        <button
+                            onClick={() => addParticipant(currentUser.userContent._id, currentUser.userContent.username, currentUser.userContent.profilePicture)}
+                            className="  my-10 border py-2 border-solid border-base-500 rounded-lg px-5 bg-secondary hover:bg-secondaryDark">Join
+                            this event
+                        </button>
+                        <div className="space-x-2 flex">
+                            <p className="space-x-2 py-0.5 pr-5">Participants: </p>
+                            {event.participants.map((participant, _index) => (
+                                <div className="flex items-center gap-3" key={_index}>
+                                    <a className="space-x-2 py-0.5">{participant.name}</a>
+                                    <div
+                                        className="flex  h-10 w-10 rounded-full overflow-hidden mr-6 border-2 border-emerald-500/50">
+                                        <img src={participant.pfp} alt="Profile"/>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-                {
-                    event.organizer === currentUser.userContent._id && <QRCodeComponent url={`${window.location.host}/handleqr/${id}`} />
-                }
+                        {
+                            event.organizer === currentUser.userContent._id &&
+                            <QRCodeComponent url={`${window.location.host}/handleqr/${id}`}/>
+                        }
 
+                    </div>
+                </div>
             </div>
         </div>
     );
